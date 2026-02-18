e096641b591baa3086a2e322acedd59146e78e85
valentin-rack <valentin.svcs@gmail.com>
2026-02-18 00:37:03 -0300

    HIROTA 2.0: github pages fix

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/hirota-2.0/',
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
+
+
+
